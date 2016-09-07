dd12ce7f62a6b26319e0865b1bf7b98613bd4de9
Mizukake <P-1406-0004@P-1406-0004-no-MacBook-Pro.local>
2016-09-07 11:07:31 +0900

    初回コミット

---
+++ ItemSelectedSample/ItemSelectedSample/ItemSelectedSamplePage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace ItemSelectedSample
+{
+	public partial class ItemSelectedSamplePage : ContentPage
+	{
+		public List<ListItem> Source { set; get; }
+		private ListItem _selectItem;
+		public ListItem SelectItem 
+		{ 
+			set 
+			{
+				_selectItem = value;
+				//何か処理する
+				_selectItem = null;
+				OnPropertyChanged("SelectItem");
+			}
+			get { return _selectItem; }
+		}
+		public ItemSelectedSamplePage()
+		{
+			InitializeComponent();
+			BindingContext = this;
+			Source = new List<ListItem>
+			{
+				new ListItem{Name="ほげ１"},
+				new ListItem{Name="ほげ２"},
+				new ListItem{Name="ほげ３"},
+				new ListItem{Name="ほげ４"},
+				new ListItem{Name="ほげ５"},
+			};
+			OnPropertyChanged("Source");
+		}
+	}
+
+	public class ListItem
+	{
+		private string _name;
+		public string Name
+		{
+			get { return _name; }
+			set { _name = value;}
+		}
+	}
+}
+
